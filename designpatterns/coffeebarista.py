01ed2cc61d41c949eff5eb9a11a17e87ad12b4e0
argonaultes <gael@argonaultes.fr>
2026-03-23 10:29:38 +0100

    bad implementation 1

--- designpatterns/coffeebarista.py
+++ designpatterns/coffeebarista.py
@@ -7,13 +7,14 @@ class Beverage(ABC):
     
     def __init__(self):
         self.milk = False
+        self.sugar = False
         
         
     def add_milk(self):
         self.milk = True
         
     def add_sugar(self):
-        ...
+        self.sugar = True
     
     @abstractmethod
     def cost(self):
@@ -24,26 +25,74 @@ class Coffee(Beverage):
     
     def __init__(self):
         super().__init__()
+        self.coffee_bean = False
     
-    def add_coffee(self):
-        ...
+    def add_coffee_bean(self):
+        self.coffee_bean = True
     
     def cost(self):
+        total_cost = 1.0
         if self.milk:
-            ...   
+            total_cost += 0.5
+        if self.sugar:
+            total_cost += 0.4
+        if self.coffee_bean:
+            total_cost += 0.2
+        return total_cost
 
 # base cost: 1.5    
 class Tea(Beverage):
-    ...
-    
+
+    def __init__(self):
+        super().__init__()
+        
+    def cost(self):
+        total_cost = 1.5
+        if self.milk:
+            total_cost += 0.5
+        if self.sugar:
+            total_cost += 0.4
+        return total_cost
+        
 # base cost: 0.7
 class Chocolate(Beverage):
-    ...
+    
+    def __init__(self):
+        super().__init__()
+        self.sha = False
+
+    def add_sha(self):
+        self.sha = True
+
+    def cost(self):
+        total_cost = 0.7
+        if self.milk:
+            total_cost += 0.5
+        if self.sugar:
+            total_cost += 0.4
+        if self.sha:
+            total_cost += 0.5
+        return total_cost
 
 # base cost: 2.0    
 class Latte(Beverage):
-    ...
-    
+
+    def __init__(self):
+        super().__init__()
+        self.sha = False
+
+    def add_sha(self):
+        self.sha = True
+
+    def cost(self):
+        total_cost = 2.0
+        if self.milk:
+            total_cost += 0.5
+        if self.sugar:
+            total_cost += 0.4
+        if self.sha:
+            total_cost += 0.5
+        return total_cost    
     
 if __name__ == '__main__':
     c = Coffee()
