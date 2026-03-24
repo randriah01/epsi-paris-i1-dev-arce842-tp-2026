ef6bff2c13ec0185c888624ab75894744aa6e244
argonaultes <gael@argonaultes.fr>
2026-03-24 09:23:08 +0100

    exercice goose

--- designpatterns/duck_simulator.py
+++ designpatterns/duck_simulator.py
@@ -4,11 +4,23 @@ class DuckSimulator:
     
     def simulate(self, duck : Duck):
         self.__fly_n_times(duck, 2)
-        duck.quack()
+        duck.quack() #ERROR: fix this in 2 lines
         self.__fly_n_times(duck, 3)
         
-    def __fly_n_times(duck : Duck, n_times : int):
+    def __fly_n_times(self, duck : Duck, n_times : int):
         for _ in range(n_times):
             duck.fly()
-        
+
+class Goose:
     
+    def honk(self):
+        print('Honk honk')
+        
+    def fly(self):
+        print('Fly like a goose')
+
+        
+if __name__ == '__main__':
+    goose = Goose()
+    simulator = DuckSimulator()
+    simulator.simulate(goose)
