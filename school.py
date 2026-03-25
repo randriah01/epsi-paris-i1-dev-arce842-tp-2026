4a60b2c9b38cc54210838304406208bdf280ff05
Princia <rfmiseheno@gmail.com@gmail.com>
2026-03-25 13:05:26 +0100

    Etape 2

--- school.py
+++ school.py
@@ -22,3 +22,10 @@ class SchoolClass:
 
     def add_student(self, student: Student):
         self.__students.append(student)
+
+
+if __name__ == '__main__':
+    school_class = SchoolClass()
+    school_class.add_student(Student('J', 10, 12, 13))
+    school_class.add_student(Student('A', 8, 2, 17))
+    school_class.add_student(Student('V', 9, 14, 14))
