5029ec68f4611729b364e1340cd6fae5db13c5e5
Princia <rfmiseheno@gmail.com@gmail.com>
2026-03-25 12:56:10 +0100

    Etape 1 - Déclaration des classes Student et SchoolClass

---
+++ school.py
@@ -0,0 +1,24 @@
+class Student:
+
+    def __init__(self, name: str, matter_1: float, matter_2: float, matter_3: float):
+        self.name = name
+        self.matter_1 = matter_1
+        self.matter_2 = matter_2
+        self.matter_3 = matter_3
+
+    @property
+    def average(self):
+        return (self.matter_1 + self.matter_2 + self.matter_3) / 3
+
+    def __str__(self):
+        return (f'Student {self.name} | M1: {self.matter_1} | M2: {self.matter_2} '
+                f'| M3: {self.matter_3} | avg: {self.average:.2f}')
+
+
+class SchoolClass:
+
+    def __init__(self):
+        self.__students = []
+
+    def add_student(self, student: Student):
+        self.__students.append(student)
